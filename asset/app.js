54439184cd1491d64de8b5591eadf40bc61f9ccb
Joseph Tawil <jtawil2015@gmail.com>
2020-05-11 07:11:00 -0700

    added another ajax request to app.js

--- asset/app.js
+++ asset/app.js
@@ -1,17 +1,16 @@
 $(document).ready(function () {
   $("#btnSubmit").on("click", function (e) {
     e.preventDefault();
-    // var api_key = $("#api_key").val();
-    var api_key = "8db0f9bc1f4b7d7c56298c24299661bf";
+    var api_key = "&apikey=8db0f9bc1f4b7d7c56298c24299661bf";
+    var cat = "&q=coffee";
+    var searchURL = "https://developers.zomato.com/api/v2.1/search?";
+    var cityURL = "https://developers.zomato.com/api/v2.1/cities?";
+    var count = "&count=5";
     var textInput = $("#textInput").val();
-    $("#textInput").val("");
-    var cat = $("#cat").val();
-    $("#limit").val("");
-    $("main").html("");
-
+    var entityId = "";
     $.ajax({
       type: "GET",
-      url: `https://developers.zomato.com/api/v2.1/search?entity_id=306&entity_type=city&q=coffee%20shop&count=3&apikey=${api_key}`,
+      url: cityURL + "q=" + textInput + api_key,
       dataType: "json",
     }).then(function (response) {
       console.log(response);
@@ -33,19 +32,11 @@ $(document).ready(function () {
         dataType: "json",
       }).then(function (dresponse) {
         console.log(dresponse);
-
         var randNum = Math.floor(Math.random() * dresponse.restaurants.length);
 
         console.log(dresponse.restaurants[randNum].restaurant.url);
 
-        // for (var i = 0; i < response.restaurants.length; i++) {
-        //   var still = response.restaurants[i].restaurant.url;
-        //   // var gif = response.restaurants[i].restaurant.original.url;
-        //   $("body").append(
-        //     `<img src=${response.restaurants[randNum].restaurant.photos[0].photo.url}>`
-        //   );
-        // }
-
+        var randNum = Math.floor(Math.random() * dresponse.restaurants.length);
         var name = dresponse.restaurants[0].restaurant.name;
         var img =
           dresponse.restaurants[randNum].restaurant.photos[0].photo.thumb_url;
