e404614bcea2d0f5afa85fc0b913776f37d5576a
ichoi21 <ihchoi@gmail.com>
2020-05-14 16:09:43 -0700

    footer fixed, learn revised with brew.js

--- asset/brew.js
+++ asset/brew.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
-  $("#brew").html(`<div class="embed-responsive embed-responsive-16by9">
+  $("#btnBrew").on("click", function (e) {
+    e.preventDefault();
+    $("#brew").html(`<div class="embed-responsive embed-responsive-16by9">
   <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/2Sela-8BF9Q" allowfullscreen></iframe>
 </div>`);
+  });
+
+  $("#btnMyth").on("click", function (e) {
+    e.preventDefault();
+    $("#myth").html(`<div class="embed-responsive embed-responsive-16by9">
+<iframe class="embed-responsive-item" src="https://www.missioncoffeeco.com/blogs/news/fact-or-myth-do-light-roasts-have-more-caffeine-than-dark-roasts"></iframe>
+</div>`);
+  });
+
+  $("#btnRoast").on("click", function (e) {
+    e.preventDefault();
+    $("#roast").html(`<div class="embed-responsive embed-responsive-16by9">
+<iframe class="embed-responsive-item" src="https://www.missioncoffeeco.com/blogs/news/fact-or-myth-do-light-roasts-have-more-caffeine-than-dark-roasts"></iframe>
+</div>`);
+  });
 });
